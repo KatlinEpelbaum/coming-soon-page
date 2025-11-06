fb750a1058a7cb07842cc215702b92bd36970e33
anette.truupold <anette.truupold@ametikool.ee>
2025-11-06 11:07:37 +0200

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="custom-shape-divider-bottom-1762416976">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+        </svg>
+    </div>
+    <div class="md:flex items-center md:px-16 px-8 pt-8 md:pt-0 pb-16 bg-[#FFDC9CFF] ">
+      <div class="md:w-1/3 space-y-2">
+        <img src="/logo.png" alt="">
+        <p> Join our lunatic <br class="hidden md:inline-block">family ✷</p>
+        <p class="mt-4"> © 2025 Lune. All rights reserved.</p>
+      </div>
+      <div class="md:w-2/3 space-x-12 mt-8 flex">
+        <a> Home </a>
+        <a> Features </a>
+        <a> Contact us </a>
+      </div>
+    </div>
+  </div>
+  </template>
+  <style scoped>
+  .custom-shape-divider-bottom-1762416976 {
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+    transform: rotate(180deg);
+  }
+  .custom-shape-divider-bottom-1762416976 svg {
+    position: relative;
+    display: block;
+    width: calc(100% + 1.3px);
+    height: 79px;
+  }
+  .custom-shape-divider-bottom-1762416976 .shape-fill {
+    fill: #FFDC9C;
+  }
+  </style>
+
+
+
+
+
+
+
